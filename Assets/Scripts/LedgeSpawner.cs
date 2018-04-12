eb2b7f8d38dd2d373756ae6ccbe0856d88b65413
Acxell Leandro <aleandro803@gmail.com>
2018-04-12 15:59:24 +1000

    Pivot working, grid set, and play area added

---
+++ Assets/Scripts/LedgeSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LedgeSpawner : MonoBehaviour {
+	public GameObject grabLedge;
+	public GameObject trapLedge;
+	Vector3[] pos;
+	public int ledgeOnGrid = 400;
+	public int trapLedgeOnGrid = 60;
+	public int gridWidth = 40;
+	public int gridHeight= 60;
+
+	// Use this for initialization
+	void Start () 
+	{
+		for (int i = 0; i < gridWidth;i++) 
+		{
+			GameObject ledge1 = Instantiate (grabLedge,new Vector3(i *2.5f,0,0), grabLedge.transform.rotation) as GameObject;
+			for (int j = 0; j < gridHeight;j++) 
+			{
+				GameObject ledge2 = Instantiate (grabLedge,new Vector3(i*2.5f,j *2.5f,0), grabLedge.transform.rotation) as GameObject;
+
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayerPivot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPivot : MonoBehaviour {
+	public Transform player;
+	public Transform player2;
+	public bool movingLeft = false;
+	public bool movingRight = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		if (Input.GetKey (KeyCode.Mouse0)) 
+		{
+			transform.RotateAround(player.position, Vector3.back, 100 * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.Mouse1)) 
+		{
+			transform.RotateAround (player2.position, Vector3.forward, 100 * Time.deltaTime);
+		}	
+	}
+}
